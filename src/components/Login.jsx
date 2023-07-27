04f842d7b370f7592a22b628169ea0a7b3f68123
Anin-Hasan <200921@ku.ac.bd>
2023-07-28 00:06:51 +0600

    noUserPopUp added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,8 +11,10 @@ const Login = () => {
   const passBox = useRef();
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
+
   const [wrongPass, setWrongPass] = useState(false);
   const [invalidEmail, setInvalidEmail] = useState(false);
+  const [noUser, setNoUser] = useState(false);
 
   const [showPassword, setShowPassword] = useState(false);
 
@@ -42,10 +44,17 @@ const Login = () => {
         if (error.code === "auth/invalid-email") {
           setInvalidEmail(true);
           setWrongPass(false);
+          setNoUser(false);
         }
         if (error.code === "auth/wrong-password") {
           setInvalidEmail(false);
           setWrongPass(true);
+          setNoUser(false);
+        }
+        if (error.code === "auth/user-not-found") {
+          setInvalidEmail(false);
+          setWrongPass(false);
+          setNoUser(true);
         }
       });
   };
@@ -243,6 +252,33 @@ const Login = () => {
                     </svg>
                   </div>
                   <h1 className="font-semibold">Invalid Email</h1>
+                </div>
+              </div>
+              <div
+                className={
+                  noUser === !true
+                    ? "relative bottom-[999px] bg-red-500 text-white p-4 rounded-md bg-opacity-90 shadow-md "
+                    : "relative bottom-[500px] bg-red-500 text-white p-4 rounded-md bg-opacity-90 shadow-md duration-300"
+                }
+              >
+                <div className="flex gap-1 items-center justify-center">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                      />
+                    </svg>
+                  </div>
+                  <h1 className="font-semibold">No Account Found</h1>
                 </div>
               </div>
             </form>
